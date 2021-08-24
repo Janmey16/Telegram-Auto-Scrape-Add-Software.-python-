046d29106c5d653f068f3d13a41ab9485e5017a1
Janmey16 <85831350+Janmey16@users.noreply.github.com>
2021-08-25 00:10:10 +0530

    Update Addingusers_bot.py

--- Addingusers_bot.py
+++ Addingusers_bot.py
@@ -6,12 +6,24 @@ updater = Updater(
     "1910773191:AAFXjLzXfjbOK1VnGqmaz6ea37noZCsURtc", use_context=True)
 dispatcher = updater.dispatcher
 
-def start(update: Update, context: CallbackContext):
+def onstart(update: Update, context: CallbackContext):
     bot.send_message(
         chat_id=update.effective_chat.id,
         text = ("Let me show you the group list. \nUse /groups command to agree")
+        bot.register_next_step_handler(text, ongroups)
+    )
+    
+def ongroups(update: Update, context: CallbackContext):
+    bot.send_message(
+        chat_id=effective_chat.id,
+        text = ("Here is the group list"),
+        
     )
         
-start_value = CommandHandler('start', start)
+start_value = CommandHandler('start', onstart)
+
 dispatcher.add_handler(start_value)
+
+start_value1 = CommandHandler('groups', ongroups)
+dipatcher.add_handler(start_value1)
 updater.start_polling()
